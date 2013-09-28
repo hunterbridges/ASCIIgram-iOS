625a28db9e751dfaebb12a1c5e9f92760dd2e2ab
Jeff Rafter <jeffrafter@gmail.com>
2013-09-27 17:46:04 -0700

    A thing that puts everything to the log

--- ASCIIgram/UI Elements/TextArtCameraView.m
+++ ASCIIgram/UI Elements/TextArtCameraView.m
@@ -1,14 +1,20 @@
 #import <AVFoundation/AVFoundation.h>
+#import <QuartzCore/QuartzCore.h>
 #import "GPUImage.h"
 #import "TextArtCameraView.h"
+
 
 @interface TextArtCameraView ()
 
 @property (nonatomic, strong) NSTimer *timer;
-@property (nonatomic, strong) GPUImageStillCamera *stillCamera;
+@property (nonatomic, strong) GPUImageVideoCamera *videoCamera;
 @property (nonatomic, strong) GPUImageFilter *filter;
 
 @end
+
+const int kcharWidth = 35;
+const int kcharHeight = 21;
+
 
 @implementation TextArtCameraView
 
@@ -39,21 +45,23 @@
   GPUImageCannyEdgeDetectionFilter *cannyFilter = [[GPUImageCannyEdgeDetectionFilter alloc] init];
   GPUImageColorInvertFilter *invertFilter = [[GPUImageColorInvertFilter alloc] init];
   
-  [sampleFilter forceProcessingAtSizeRespectingAspectRatio:CGSizeMake(512, 512)];
-  [preparedFilter addTarget:sampleFilter];
-  [sampleFilter addTarget:cannyFilter];
-  [cannyFilter addTarget:invertFilter];
+  [sampleFilter forceProcessingAtSize:CGSizeMake(kcharWidth, kcharHeight)];
+
+  [preparedFilter addTarget:cannyFilter];
+  [cannyFilter addTarget:sampleFilter];
+  [sampleFilter addTarget:invertFilter];
   
   self.filter = invertFilter;
   
   // Create custom GPUImage camera
-  self.stillCamera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPresetLow cameraPosition:AVCaptureDevicePositionBack];
+  self.videoCamera = [[GPUImageVideoCamera alloc]
+                      initWithSessionPreset:AVCaptureSessionPresetLow cameraPosition:AVCaptureDevicePositionBack];
                       
-  self.stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
-  [self.stillCamera addTarget:preparedFilter];
+  self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+  [self.videoCamera addTarget:preparedFilter];
   
   // Begin showing video camera stream
-  [self.stillCamera startCameraCapture];
+  [self.videoCamera startCameraCapture];
   
   // Get that
   [self sampleCamera];
@@ -62,18 +70,91 @@
 
 - (void)sampleCamera {
   self.timer = nil;
+  NSLog(@"Processing");
   
-  [self.stillCamera capturePhotoAsImageProcessedUpToFilter:self.filter
-                                     withCompletionHandler:^(UIImage *processedImage, NSError *error){
-                                       NSLog(@"Processing");
-                                       self.asciiImageView.image = processedImage;
-                                       self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1
-                                                                                     target:self
-                                                                                   selector:@selector(sampleCamera)
-                                                                                   userInfo:nil
-                                                                                    repeats:NO];
-                                     }];
+  UIImage *image = [self.filter imageFromCurrentlyProcessedOutput];
+  
+  NSData* pixelData = (__bridge NSData*) CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage));
+  unsigned char* pixelBytes = (unsigned char*)[pixelData bytes];
+  
+  NSMutableString *m = [[NSMutableString alloc] init];
+//  char *pixelChars = calloc((pixelData.length / 4) + 1, sizeof(char));
+  int index = 0;
+  int lines = 0;
+  // Take away the red pixel, assuming 32-bit RGBA
+  for(int i = 0; i < pixelData.length; i += 4) {
+    uint8_t red = pixelBytes[i];
+    if (index++ % kcharWidth == 0)  {
+      lines++;
+      [m appendString:@"\n"];
+    }
+    [m appendFormat:@"%c", [self charForPixel:red]];
+  }
+  
+  NSLog(@"Pix (tot: %i, chars: %i, lines: %i):\n %@", pixelData.length, index, lines, m);
+  
+  self.asciiImageView.image = image;
+  
+  // Use a timer here to get a slight pause and to not overflow the stack
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5
+                                                target:self
+                                              selector:@selector(sampleCamera)
+                                              userInfo:nil
+                                               repeats:NO];
 }
 
+static char BLACK = '@';
+static char CHARCOAL = '#';
+static char DARKGRAY = '8';
+static char MEDIUMGRAY = '&';
+static char MEDIUM = 'o';
+static char GRAY = ':';
+static char SLATEGRAY = '*';
+static char LIGHTGRAY = '.';
+static char WHITE = ' ';
+
+- (char)charForPixel:(int)redValue
+{
+  char asciival = ' ';
+  
+  if (redValue >= 230)
+  {
+    asciival = WHITE;
+  }
+  else if (redValue >= 200)
+  {
+    asciival = LIGHTGRAY;
+  }
+  else if (redValue >= 180)
+  {
+    asciival = SLATEGRAY;
+  }
+  else if (redValue >= 160)
+  {
+    asciival = GRAY;
+  }
+  else if (redValue >= 130)
+  {
+    asciival = MEDIUM;
+  }
+  else if (redValue >= 100)
+  {
+    asciival = MEDIUMGRAY;
+  }
+  else if (redValue >= 70)
+  {
+    asciival = DARKGRAY;
+  }
+  else if (redValue >= 50)
+  {
+    asciival = CHARCOAL;
+  }
+  else
+  {
+    asciival = BLACK;
+  }
+  
+  return asciival;
+}
 
 @end
